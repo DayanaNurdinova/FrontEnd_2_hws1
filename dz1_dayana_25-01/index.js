f15f8ba68c7cfee8bcfbe3b5f971fe2ed147a812
DayanaNurdinova <130187491+DayanaNurdinova@users.noreply.github.com>
2023-04-08 03:54:29 +0600

    Выполнила 1 дз (25-01)

---
+++ dz1_dayana_25-01/index.js
@@ -0,0 +1,13 @@
+var age = Number(prompt('Enter your assessment!'))
+if ( age > 5 ) {
+    alert('Thank you for assessment!')
+} else if ( age < 5) {
+    alert('We are working on improving the service!')
+} else {
+    alert('ERROR')
+}
+
+
+var lang = 'EN'
+       var arr = ['Mn','Ts','Wd','Th','Fr','St','Sn'];
+console.log(arr)
